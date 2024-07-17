fd36846bd38b86aee05a3128277322c8982c18e3
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-07-17 19:21:31 +0200

    Completato esercizio! (MILESTONE 3)

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,18 @@ const imagesContainer = document.querySelector('.images-container');
 
 imagesContainer.innerHTML += `   
     <div class="prev">
-        <button id="prev">Prev</button>
+        <button id="prev" class="my-btn">
+        <i class="fa-solid fa-chevron-left"></i>
+        </button>
     </div>
 
     <div class="next">
-        <button id="next">Next</button>
+        <button id="next" class="my-btn">
+        <i class="fa-solid fa-chevron-right"></i>
+        </button>
     </div>
 
-    <div class="single-image active">
+    <div class="single-image">
         <img src="${images[0]}" alt="01">
     </div>
 
@@ -38,6 +42,42 @@ imagesContainer.innerHTML += `
         <img src="${images[4]}" alt="05">
     </div>
 `;
+
+const prevButton = document.getElementById('prev');
+
+const nextButton = document.getElementById('next');
+
+let currentIndex = 0;
+
+const allImages = document.querySelectorAll('.single-image');
+
+prevButton.addEventListener('click', 
+    function() {
+        allImages[currentIndex].classList.remove('active');
+        currentIndex--;
+
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        }
+
+        allImages[currentIndex].classList.add('active');
+    }
+);
+
+nextButton.addEventListener('click', 
+    function() {
+        allImages[currentIndex].classList.remove('active');
+        currentIndex++;
+
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+
+        allImages[currentIndex].classList.add('active');
+    }
+);
+
+allImages[currentIndex].classList.add('active');
 
 
 
